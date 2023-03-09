cfd2ea06305d69eda4997ee7b9aaba40d34f3010
Jumanah Almajnouni <jumanahh@outlook.com>
2023-03-09 11:28:41 -0800

    Updated Board class with methods from UML

--- Board.py
+++ Board.py
@@ -2,3 +2,13 @@
 class Board:
     def __init__(self) -> None:
         pass
+    def resetBoard():
+        pass
+    def createBoard():
+        pass
+    def updateBoard():
+        pass
+    def isValidSwapTile(tile, direction):
+        pass
+    def removeTile(tiles):
+        pass
