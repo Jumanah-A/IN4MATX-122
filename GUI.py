1aa8ea16968334023d1a74457fb524dc52f3b3b2
Andy Lee <leeah4@uci.edu>
2023-03-23 22:39:05 -0700

    temporary commit

--- GUI.py
+++ GUI.py
@@ -10,6 +10,7 @@ from GUI_Design import GUI_Design
 # LEFT = 'left'
 # RIGHT = 'right'
 
+
 class GUI:
     def __init__(self):
         # board width and board height will change upon current game being set
@@ -103,33 +104,11 @@ class GUI:
             f'Total Moves: {self.currentGame.moveCount}', True, self.design.SCORECOLOR)
         self.DISPLAYSURF.blit(score_text, (400, 10))
 
-    def startCandyCrush1P(self):
-        # self.DISPLAYSURF = self.pygame.display.set_mode((1000, 600))
-        game = CandyCrush(1, self)
-        self._startGame(game)
-
-    def startCandyCrush2P(self):
-        game = CandyCrush(2, self)
-        self._startGame(game)
-
-    def startBejeweled1P(self):
-        # FIX LATER
-        # self.DISPLAYSURF = self.pygame.display.set_mode((1000, 600))
-        game = Bejeweled(1, self)
-        self._startGame(game)
-
-    def startBejeweled2P(self):
-        # FIX LATER
-        # self.DISPLAYSURF = self.pygame.display.set_mode((1000, 600))
-        game = Bejeweled(2, self)
-        self._startGame(game)
-
     # private helper function
-    def _startGame(self, game):
+    def setGameAttributes(self, game):
         self.currentGame = game
         self.BOARDHEIGHT = self.currentGame.board.numRows
         self.BOARDWIDTH = self.currentGame.board.numCols
-        game.start()
 
     def drawBoard(self, board, turn=-1):
         self.DISPLAYSURF = self.pygame.display.set_mode(
@@ -172,7 +151,8 @@ class GUI:
     def drawFinalScore(self, score, player=-1):
         if player == -1:
             self.clickContinueTextSurf = self.BASICFONT.render(
-                'Final Score: %s! Press space to return to menu' % (score), 1, self.design.GAMEOVERCOLOR,
+                'Final Score: %s! Press space to return to menu' % (
+                    score), 1, self.design.GAMEOVERCOLOR,
                 self.design.GAMEOVERBGCOLOR)
         elif player != '':
             self.clickContinueTextSurf = self.BASICFONT.render('Winner: %s, Score: %s! Press space to return to menu' % (
--- Menu.py
+++ Menu.py
@@ -17,22 +17,42 @@
 
 # if __name__ == "__main__":
 #     main()
+from Bejeweled import Bejeweled
+from CandyCrush import CandyCrush
+from functools import partial
 import pygame_menu
 
 
 class Menu:
     def __init__(self, GUI) -> None:
-        my_theme = pygame_menu.themes.THEME_BLUE.copy()
-        my_theme.title_font = pygame_menu.font.FONT_FRANCHISE
-        my_theme.widget_font = pygame_menu.font.FONT_FRANCHISE
-        self.menu = pygame_menu.Menu(
-            'INF122 FINAL PROJECT', 1000, 600, theme=my_theme)
-
-        self.menu.add.button('Play Candy Crush 1P', GUI.startCandyCrush1P)
-        self.menu.add.button('Play Candy Crush 2P', GUI.startCandyCrush2P)
-        self.menu.add.button('Play Bejeweled 1P', GUI.startBejeweled1P)
-        self.menu.add.button('Play Bejeweled 2P', GUI.startBejeweled2P)
-        self.menu.add.button('Quit', pygame_menu.events.EXIT)
+        self.theme = pygame_menu.themes.THEME_BLUE.copy()
+        self.theme.title_font = pygame_menu.font.FONT_FRANCHISE
+        self.theme.widget_font = pygame_menu.font.FONT_FRANCHISE
+        self.GUI = GUI
+        self.mainMenu()
 
     def mainloop(self, surface):
         self.menu.mainloop(surface)
+
+    def chooseGame(self, game):
+        self.menu = pygame_menu.Menu(
+            'INF122 FINAL PROJECT', 1000, 600, theme=self.theme)
+        self.menu.add.button('1 Player', partial(
+            self.startGame, playerCount=1))
+        self.menu.add.button('2 Player', partial(
+            self.startGame, playerCount=2))
+        self.menu.add.button('Go Back', self.mainMenu)
+        self.mainLoop(self.GUI.DISPLAYSURF)
+
+    def startGame(self, game, playerCount):
+        gameInstance = game(playerCount, self.GUI)
+        self.GUI.setGame(gameInstance)
+
+    def mainMenu(self):
+        self.menu = pygame_menu.Menu(
+            'INF122 FINAL PROJECT', 1000, 600, theme=self.theme)
+        self.menu.add.button('Play Candy Crush', partial(
+            self.chooseGame, game=CandyCrush))
+        self.menu.add.button('Play Bejeweled', partial(
+            self.chooseGame, game=Bejeweled))
+        self.menu.add.button('Quit', pygame_menu.events.EXIT)
